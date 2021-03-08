e9257b8f0ad90f358a614b7c94ebe79b5c0bd323
darktower <cuneyt.cuneyitoglu@fortanix.com>
2021-03-09 00:42:49 +0100

    Second

--- vars/killOldBuilds.groovy
+++ vars/killOldBuilds.groovy
@@ -1,10 +1,30 @@
-#!/usr/bin/env groovy
+@NonCPS
+private void abortBuild(build){
+    boolean aborted=false;
+    if (build instanceof org.jenkinsci.plugins.workflow.job.WorkflowRun){
+        int counter=0
+        while (counter<60 && build.isInProgress()){
+            for (org.jenkinsci.plugins.workflow.support.steps.input.InputAction inputAction:build.getActions(org.jenkinsci.plugins.workflow.support.steps.input.InputAction.class)){
+                for (org.jenkinsci.plugins.workflow.support.steps.input.InputStepExecution inputStep:inputAction.getExecutions()){
+                    if (!inputStep.isSettled()){
+                        inputStep.doAbort()
+                    }
+                }
+            }
+            
+            counter++
+            Thread.sleep(1000) //milliseconds
+        }
+    }
 
-import hudson.model.Result
-import jenkins.model.CauseOfInterruption.UserInterruption
+    if (build.isInProgress()){
+        build.doKill()
+    }
 
-def killOldBuilds() {
-  while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
-    currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
-  }
+}
+
+def call(currentBuild) {
+    while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
+        abortBuild(currentBuild.rawBuild.getPreviousBuildInProgress())
+    }
 }
