76518f1ab82eb099a3c8bcc5ade7adc21fe9d15d
Cuneyt Cuneyitoglu <cuneyt.cuneyitoglu@fortanix.com>
2021-03-08 23:27:38 +0100

    First

---
+++ vars/killOldBuilds.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+import hudson.model.Result
+import jenkins.model.CauseOfInterruption.UserInterruption
+
+def killOldBuilds() {
+  while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
+    currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
+  }
+}
